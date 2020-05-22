79775b0e8917c17bbde16967e8cb4b8b6da714ce
tdkavanagh <65189502+tdkavanagh@users.noreply.github.com>
2020-05-22 00:27:26 -0400

    Create BackendFeedbackForm.php

---
+++ BackendFeedbackForm.php
@@ -0,0 +1,16 @@
+<?php
+
+if (isset($_POST['Submit'])) {
+  $name = $_POST['name'];
+  $Subject = $_POST['Subject'];
+  $mailFrom = $_POST['e-mail'];
+  $message = $_POST['message'];
+
+
+  $mailTo = "tyler_kavanagh@yahoo.com";
+  $headers = "From: ".$mailFrom;
+  $txt = "You have recieved an e-mail from Budgetbooking Customer Feedback form ".$name.". \n\n" .$message;
+
+  mail($mailTo, $Subject, $txt, $headers);
+  header("location: index.php?mailsend");
+}

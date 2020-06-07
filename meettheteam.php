382f309d8ddb70b23b14a39c7f2b12d51a8e2817
tdkavanagh <65189502+tdkavanagh@users.noreply.github.com>
2020-06-06 23:35:05 -0400

    Create meettheteam.php

---
+++ meettheteam.php
@@ -0,0 +1,98 @@
+
+<section class="team-section my-5">
+
+  
+  <h2 class="h1-responsive font-weight-bold text-center my-5">Our Design team</h2>
+
+  <p class="grey-text text-center w-responsive mx-auto mb-5">Meet the exciting Budget Bookkeeping Team!</p>
+
+
+  <div class="row text-center text-md-left">
+
+  
+    <div class="col-xl-6 col-lg-12 mb-5 d-md-flex justify-content-between">
+      <div class="avatar mb-md-0 mb-4 mx-4">
+        <img src="https://mdbootstrap.com/img/Photos/Avatars/img%20(32).jpg" class="rounded z-depth-1" alt="Sample avatar">
+      </div>
+      <div class="mx-4">
+        <h4 class="font-weight-bold mb-3">Jessica Mccoy</h4>
+        <h6 class="font-weight-bold grey-text mb-3">Project Manager </h6>
+        <p class="grey-text">Text goes here</p>
+      </div>
+    </div>
+
+
+
+    <div class="col-xl-6 col-lg-12 mb-5 d-md-flex justify-content-between">
+      <div class="avatar mb-md-0 mb-4 mx-4">
+        <img src="https://mdbootstrap.com/img/Photos/Avatars/img%20(32).jpg" class="rounded z-depth-1" alt="Sample avatar">
+      </div>
+      <div class="mx-4">
+        <h4 class="font-weight-bold mb-3">Dawood Shammas</h4>
+        <h6 class="font-weight-bold grey-text mb-3">Lead Web Developer</h6>
+        <p class="grey-text">Text goes here</p>
+
+
+  </div>
+
+
+  <div class="row text-center text-md-left">
+
+  
+    <div class="col-xl-6 col-lg-12 mb-xl-0 mb-5 d-md-flex justify-content-between">
+      <div class="avatar mb-md-0 mb-4 mx-4">
+        <img src="https://mdbootstrap.com/img/Photos/Avatars/img%20(26).jpg" class="rounded z-depth-1" alt="Sample avatar">
+      </div>
+      <div class="mx-4">
+        <h4 class="font-weight-bold mb-3">Maurice Fuentes</h4>
+        <h6 class="font-weight-bold grey-text mb-3">Backend Developer</h6>
+        <p class="grey-text">Text goes here</p>
+      </div>
+    </div>
+  
+
+  
+    <div class="col-xl-6 col-lg-12 d-md-flex justify-content-between">
+      <div class="avatar mb-md-0 mb-4 mx-4">
+        <img src="https://mdbootstrap.com/img/Photos/Avatars/img%20(29).jpg" class="rounded z-depth-1 img-fluid"
+          alt="Sample avatar">
+      </div>
+      <div class="mx-4">
+        <h4 class="font-weight-bold mb-3">Qasim Rathur</h4>
+        <h6 class="font-weight-bold grey-text mb-3">Web Developer</h6>
+        <p class="grey-text">Text goes here</p>
+      </div>
+    </div>
+    
+
+    
+    <div class="col-xl-6 col-lg-12 d-md-flex justify-content-between">
+      <div class="avatar mb-md-0 mb-4 mx-4">
+        <img src="https://mdbootstrap.com/img/Photos/Avatars/img%20(29).jpg" class="rounded z-depth-1 img-fluid"
+          alt="Sample avatar">
+      </div>
+      <div class="mx-4">
+        <h4 class="font-weight-bold mb-3">Samantha Belz</h4>
+        <h6 class="font-weight-bold grey-text mb-3">Web Developer</h6>
+        <p class="grey-text">Text goes here</p>
+      </div>
+    </div>
+    
+
+  
+    <div class="col-xl-6 col-lg-12 d-md-flex justify-content-between">
+      <div class="avatar mb-md-0 mb-4 mx-4">
+        <img src="https://mdbootstrap.com/img/Photos/Avatars/img%20(29).jpg" class="rounded z-depth-1 img-fluid"
+          alt="Sample avatar">
+      </div>
+      <div class="mx-4">
+        <h4 class="font-weight-bold mb-3">Tyler Kavanagh</h4>
+        <h6 class="font-weight-bold grey-text mb-3">Web Developer</h6>
+        <p class="grey-text">Text goes here</p>
+      </div>
+    </div>
+    
+  </div>
+
+
+</section>

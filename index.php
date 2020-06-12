76619f337a0772f5ea7def3a3fbe05a833c93273
tdkavanagh <65189502+tdkavanagh@users.noreply.github.com>
2020-06-12 12:52:03 -0400

    Update index.php

--- index.php
+++ index.php
@@ -206,25 +206,10 @@
             <div class="col-md-4">
                 <p>Fan? Drop a note.</p>
                 <p><span class="glyphicon glyphicon-map-marker"></span>Michigan, US</p>
-                <p><span class="glyphicon glyphicon-phone"></span>Phone: +00 1515151515</p>
-                <p><span class="glyphicon glyphicon-envelope"></span>Email: mail@mail.com</p>
+                <p><span class="glyphicon glyphicon-phone"></span>Phone: +1-202-555-0123</p>
+                <p><span class="glyphicon glyphicon-envelope"></span>Email: budgetbookkeeping20@gmail.com</p>
             </div>
-            <div class="col-md-8">
-                <div class="row">
-                    <div class="col-sm-6 form-group">
-                        <input class="form-control" id="name" name="name" placeholder="Name" type="text" required>
-                    </div>
-                    <div class="col-sm-6 form-group">
-                        <input class="form-control" id="email" name="email" placeholder="Email" type="email" required>
-                    </div>
-                </div>
-                <textarea class="form-control" id="comments" name="comments" placeholder="Comment" rows="5"></textarea>
-                <div class="row">
-                    <div class="col-md-12 form-group">
-                        <button class="btn pull-right" type="submit">Send</button>
-                    </div>
-                </div>
-            </div>
+   <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeLQYFcQGt69_kyORl2iLlT5I82uAs6v3GBlgDA9-oAaPolxQ/viewform?embedded=true" width="640" height="975" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>       
         </div>
     </div>
     <br>

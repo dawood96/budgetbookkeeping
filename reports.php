2028fb0e6a8750a69656a2e304c4c8156ea0fe18
Maurice Fuentes <mauricefuentes@oakland.edu>
2020-06-14 21:01:55 -0400

    Added reports feature
    you can look up for reports based on dates.

--- reports.php
+++ reports.php
@@ -57,17 +57,122 @@
   </div>
 </nav>
 
+<div class="container mt-md-5 text-center text-dark">
+        <h1> Reports</h1>
+    </div>
+</br>
+    <script src='jquery-3.3.1.js' type='text/javascript'></script>
+   <script src='jquery-ui.min.js' type='text/javascript'></script>
+   <script type='text/javascript'>
+   $(document).ready(function(){
+     $('.dateFilter').datepicker({
+        dateFormat: "yy-mm-dd"
+     });
+   });
+   </script>
+
+    <form method='post' action=''>
+     Start Date <input type='text' class='dateFilter' name='fromDate' placeholder="YYYY-MM-DD" value='<?php if(isset($_POST['fromDate'])) echo $_POST['fromDate']; ?>'>
+     End Date <input type='text' class='dateFilter' name='endDate' placeholder="YYYY-MM-DD" value='<?php if(isset($_POST['endDate'])) echo $_POST['endDate']; ?>'>
+     <input type='submit' name='but_search' value='Search'>
+   </form>
+   </br>
+   <button onclick="window.print();return false;">Print</button>
+   </br></br>
+   <?php
+       //$employeesRecords= "";
+       $emp_query = "SELECT * FROM csi3370_income_trans WHERE 1 ";
+       $emp_query1 = "SELECT * FROM csi3370_expenses_trans WHERE 1 ";
+
+       // Date filter
+       if(isset($_POST['but_search'])){
+          $fromDate = $_POST['fromDate'];
+          $endDate = $_POST['endDate'];
+
+          if(!empty($fromDate) && !empty($endDate)){
+             $emp_query .= " and timestamp between '".$fromDate."' and '".$endDate."' ";
+             $emp_query1 .= " and timestamp between '".$fromDate."' and '".$endDate."' ";
+          }
+        }
+
+        // Sort
+        $emp_query .= " ORDER BY timestamp DESC";
+        $emp_query1 .= " ORDER BY timestamp DESC";
+        $IncomeReports = mysqli_query($conn,$emp_query);
+        $ExpenseReports = mysqli_query($conn,$emp_query1);
+
+        // Check records found or not
+        if(mysqli_num_rows($IncomeReports) > 0){
+          echo "<h3>Income</h3>";
+          //echo "<table style='margin-left: 500px; margin-right: auto;'>";
+          echo "<table style='margin-left: auto; margin-right: auto; width:50%;'><tr style='background-color:gray;'><th  style='padding: 5px;'>Income ID</th><th>Date</th><th>Amount</th><th>Comment</th></tr>";
+          while($empRecord = mysqli_fetch_assoc($IncomeReports)){
+            $id = $empRecord['income_trans_id'];
+            $user_id = $empRecord['user_id'];
+            $date = $empRecord['timestamp'];
+            $amount = $empRecord['income'];
+            $comment = $empRecord['comment'];
+
+            //echo "<table>";
+            echo "<tr>";
+            echo "<td  style='padding: 5px;'>". $id ."</td>";
+            echo "<td>". $date ."</td>";
+            echo "<td>". $amount ."</td>";
+            echo "<td>". $comment ."</td>";
+            echo "</tr>";
+            //echo "</table>";
+          }
+        }else{
+          echo "<tr>";
+          echo "<td colspan='4'>No record found.</td>";
+          echo "</tr>";
+        }
+        echo "</table>";
+          
+        if(mysqli_num_rows($ExpenseReports) > 0){
+          echo "</br>";
+          echo "</br>";
+          echo "<h3>Expenses</h3>";
+          //echo "<table>";
+          echo "<table style='margin-left: auto; margin-right: auto; width:50%;'><tr style='background-color:gray;'><th   style='padding: 5px;'>Expense ID</th><th>Date</th><th>Amount</th><th>Comment</th><th>Type</th></tr>";
+          while($empRecord = mysqli_fetch_assoc($ExpenseReports)){
+            $id = $empRecord['expense_trans_id'];
+            $user_id = $empRecord['user_id'];
+            $date = $empRecord['timestamp'];
+            $amount = $empRecord['expense'];
+            $comment = $empRecord['comment'];
+            $type = $empRecord['type_of_expense'];
+
+            //echo "<table>";
+            echo "<tr>";
+            echo "<td  style='padding: 5px;'>". $id ."</td>";
+            echo "<td>". $date ."</td>";
+            echo "<td>". $amount ."</td>";
+            echo "<td>". $comment ."</td>";
+            echo "<td>". $type ."</td>";
+            echo "</tr>";
+            //echo "</table>";
+          }
+        }else{
+          echo "<tr>";
+          echo "<td colspan='4'>No record found.</td>";
+          echo "</tr>";
+        }
+        echo "</table>";
+        ?>
+
+
     <div class="container mt-md-5 text-center text-dark">
-        <h1> Search for transactions</h1>
+        <h2> Search for transactions</h2>
     </div>
 
     <form action="reports.php" data-ajax="false" method="POST">
-        <input name="search" type="search" autofocus>
+        <input name="search" type="search">
         <input type="submit" name="button" value="search">
     </form>
     </br>
     <button onclick="window.print();return false;">Print</button>
-
+    </br></br>
     <?php 
 
         if(isset($_POST['button'])){
